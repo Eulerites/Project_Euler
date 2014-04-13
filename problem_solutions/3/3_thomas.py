274c6b36d18d46bebd728d42d9b155e6e786263a
Thomas Mitchell <trmitchell7@gmail.com>
2014-04-12 23:59:17 -0700

    simplified it a lot, it may be slightly less efficient, but it makes way more sense logically

--- problem_solutions/3/3_thomas.py
+++ problem_solutions/3/3_thomas.py
@@ -4,20 +4,15 @@
 # What is the largest prime factor of the number 600851475143 ?
 
 
-# this basically just builds a factor tree
-# it stops after it hits a non prime or when it's iterated over half the goal
-def primes():
-    goal = 600851475143
+def primes(goal):
     factors = []
-    factor = 3.0
-    if goal % 2.0 == 0: factors = [2]
-    while factor * factor <= goal:
+    factor = 2.0
+
+    while factor <= goal:
         if goal % factor == 0:
-            for i in factors:
-                if factor / i % 1 == 0:
-                    return factors
+            goal /= factor
             factors.append(int(factor))
-        factor += 2
-    if goal / factors[-1] % 1 == 0 : factors.append(goal / factors[-1])
+        factor += 1
     return factors
-print primes()[-1]
+
+print primes(600851475143)
