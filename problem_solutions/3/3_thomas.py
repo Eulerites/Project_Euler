40fc45a9cc7b943c7a488a243149625ea43c12ef
Thomas Mitchell <trmitchell7@gmail.com>
2014-04-12 23:27:20 -0700

    fixed it to handle even numbers better, optimized a few other things

--- problem_solutions/3/3_thomas.py
+++ problem_solutions/3/3_thomas.py
@@ -3,17 +3,21 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
+
+# this basically just builds a factor tree
+# it stops after it hits a non prime or when it's iterated over half the goal
 def primes():
     goal = 600851475143
     factors = []
     factor = 3.0
+    if goal % 2.0 == 0: factors = [2]
     while factor * factor <= goal:
         if goal % factor == 0:
             for i in factors:
-                x = factor / i
-                if x % i % 1 == 0:
+                if factor / i % 1 == 0:
                     return factors
             factors.append(int(factor))
         factor += 2
-    print factors
+    if goal / factors[-1] % 1 == 0 : factors.append(goal / factors[-1])
+    return factors
 print primes()[-1]
